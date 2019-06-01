a99a9e1ac8704db53dcd2e2ccd30f08453029600
Sai Sankeerth <sankeerth2292@gmail.com>
2019-06-01 17:53:31 +0530

    refs #3 database & models added using sqlalchemy in flask commit

--- flask_blog_app.py
+++ flask_blog_app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Initialization of the Flask Class similar to the one used in ExpressJs(NodeJs)
 app = Flask(__name__)
@@ -7,6 +9,31 @@ app = Flask(__name__)
 # Configuration of Secret Key will protect the site from modifying cookies
 # CrossSite Forgery Attacks
 app.config['SECRET_KEY'] = '4d5521851675d51736e0b3a05ad9c5f2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return 'User({},{},{})'.format(self.username,self.email,self.image_file)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    date_poster = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return 'Post({},{})'.format(self.title,self.date_poster)
 
 posts = [
     {
