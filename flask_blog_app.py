2b7fffd41a74c8cadec8222295ec758a55c496e3
Sai Sankeerth <sankeerth2292@gmail.com>
2019-06-02 16:48:29 +0530

    refs #4 Circular Import Error reproduce commit

--- flask_blog_app.py
+++ flask_blog_app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from models import User, Post
 
 # Initialization of the Flask Class similar to the one used in ExpressJs(NodeJs)
 app = Flask(__name__)
@@ -13,27 +14,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return 'User({},{},{})'.format(self.username,self.email,self.image_file)
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(300), nullable=False)
-    date_poster = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return 'Post({},{})'.format(self.title,self.date_poster)
 
 posts = [
     {
