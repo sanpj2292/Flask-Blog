3ebebb601c9e166e56801ea53e93526a734a9b37
Sai Sankeerth <sankeerth2292@gmail.com>
2019-06-03 08:00:19 +0530

    refs #12 Login Authentication Addition, modifications commit

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,9 @@
 # Initialization of Flask Blog Application
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+# For management of loginsession(s)
+from flask_login import LoginManager
 
 # Initialization of the Flask Class similar to the one used in ExpressJs(NodeJs)
 app = Flask(__name__)
@@ -9,5 +12,9 @@ app = Flask(__name__)
 # CrossSite Forgery Attacks
 app.config['SECRET_KEY'] = '4d5521851675d51736e0b3a05ad9c5f2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+# The login is the function-name of the route not the view-name
+login_manager.login_view = 'login'
 
 db = SQLAlchemy(app)
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,19 +1,33 @@
 # WT Form is an extension
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, InputRequired
+from wtforms.validators import DataRequired, Length, Email, EqualTo, InputRequired, ValidationError
+from flaskblog.models import User
 
 # Here classes are defined which will be converted to HTML
 
 # Register Form class - Loads with html tags some-styling needs to be done
 class RegistrationForm(FlaskForm):
-    username = StringField('UserName', validators=[DataRequired(), InputRequired(),
-            Length(min=2,max=50)])
+    username = StringField('UserName', validators=[DataRequired(), InputRequired(), Length(min=2,max=50)])
     email=StringField('Email', validators=[DataRequired(), InputRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), InputRequired(),])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), InputRequired(),
-            EqualTo('password')])
+    password = PasswordField('Password', validators=[DataRequired(), InputRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), InputRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    
+    #  Custom Validation for Username
+    def validate_username(self, username):
+        # username is a wtforms object
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(f'User {username.data}: Already Taken. Choose Another one')
+    
+    #  Custom Validation for Email
+    def validate_email(self, email):
+        # email is a wtforms object
+        emailId = User.query.filter_by(email=email.data).first()
+        if emailId:
+            raise ValidationError(f'Email {email.data}: Already Taken. Choose Another one')
+
 
 # Login Form class - Loads with html tags some-styling needs to be done
 class LoginForm(FlaskForm):
